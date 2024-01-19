78b411da3a1861d077cacf6eaf5c228e291b169c
DumakIt <odumak4411@gmail.com>
2024-01-19 23:18:21 +0900

    240119 header 레이아웃 표시 페이지 등록

--- src/components/commons/layout/layout.tsx
+++ src/components/commons/layout/layout.tsx
@@ -1,10 +1,13 @@
+import { useRouter } from "next/router";
 import LayoutHeader from "./header/layoutHeader";
 import { ILayoutProps } from "./layoutTypes";
 
 export default function Layout(props: ILayoutProps): JSX.Element {
+  const router = useRouter();
+
   return (
     <>
-      <LayoutHeader />
+      {router.asPath !== "/" && <LayoutHeader />}
       {props.children}
     </>
   );
