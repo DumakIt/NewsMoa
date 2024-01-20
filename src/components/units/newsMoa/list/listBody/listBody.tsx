e44fab4aab7e596cc223f54a7f804bd7f05e021c
DumakIt <odumak4411@gmail.com>
2024-01-20 21:39:40 +0900

    240120 list 페이지 무한 스크롤시 뉴스 로드중 표시

--- src/components/units/newsMoa/list/listBody/listBody.tsx
+++ src/components/units/newsMoa/list/listBody/listBody.tsx
@@ -38,36 +38,36 @@ export default function ListBody(): JSX.Element {
   return (
     <S.Container>
       <S.SelectCountry>{countriesData[selectCountry].name}</S.SelectCountry>
-      <InfiniteScroll hasMore={true} loadMore={loadMore}>
-        {translateData[selectCountry] ? (
-          translateData[selectCountry]?.map((el) => (
-            <S.NewsArticle
-              key={el.url}
-              onClick={
-                // url에 "/"가 있으면 거기까지를 경로로 인식해서 문제가 생기기 때문에 "/"가 아닌 다른 문자로 변경
-                // 해당 페이지에서 다시 "/"로 변경할 예정이기 때문에 다른 url이랑 겹치지 않는 문자로 변경
-                onClickMovePage(`/${el.url.replace(/\//g, "-_no_slash_-")}`)
-              }
-            >
-              <div>
-                <S.NewsImg
-                  referrerPolicy="no-referrer"
-                  src={el.image}
-                  alt="뉴스 이미지"
-                  onError={(event: SyntheticEvent<HTMLImageElement, Event>) =>
-                    (event.currentTarget.src = "/logoWhite.svg")
-                  }
-                />
-              </div>
-              <div>
-                <S.NewsTitle>{el.title}</S.NewsTitle>
-                <S.NewsDescription>{el.description}</S.NewsDescription>
-              </div>
-            </S.NewsArticle>
-          ))
-        ) : (
-          <S.LoadingText>뉴스 로딩중...</S.LoadingText>
-        )}
+      <InfiniteScroll
+        hasMore={true}
+        loadMore={loadMore}
+        loader={<S.LoadingText>뉴스 로딩중...</S.LoadingText>}
+      >
+        {translateData[selectCountry]?.map((el) => (
+          <S.NewsArticle
+            key={el.url}
+            onClick={
+              // url에 "/"가 있으면 거기까지를 경로로 인식해서 문제가 생기기 때문에 "/"가 아닌 다른 문자로 변경
+              // 해당 페이지에서 다시 "/"로 변경할 예정이기 때문에 다른 url이랑 겹치지 않는 문자로 변경
+              onClickMovePage(`/${el.url.replace(/\//g, "-_no_slash_-")}`)
+            }
+          >
+            <div>
+              <S.NewsImg
+                referrerPolicy="no-referrer"
+                src={el.image}
+                alt="뉴스 이미지"
+                onError={(event: SyntheticEvent<HTMLImageElement, Event>) =>
+                  (event.currentTarget.src = "/logoWhite.svg")
+                }
+              />
+            </div>
+            <div>
+              <S.NewsTitle>{el.title}</S.NewsTitle>
+              <S.NewsDescription>{el.description}</S.NewsDescription>
+            </div>
+          </S.NewsArticle>
+        ))}
       </InfiniteScroll>
     </S.Container>
   );
